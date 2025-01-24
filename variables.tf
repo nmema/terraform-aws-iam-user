3648b950ec9b3138fff664ee649f92c22a85f4db
Nicolas Mema <nicolas.a.mema@gmail.com>
2025-01-24 12:57:34 -0300

    #3 add iam user

--- variables.tf
+++ variables.tf
@@ -7,3 +7,8 @@ variable "bucket_name" {
   description = "Name of the S3 Bucket"
   type        = string
 }
+
+variable "iam_user_name" {
+  description = "Username of the IAM User"
+  type        = string
+}
